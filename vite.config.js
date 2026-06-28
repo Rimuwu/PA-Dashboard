d1c1218b987f0b4efafd5845c22c0deaaed79727
Arseniy <arseniykofanov@gmail.com>
2026-06-28 10:32:16 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'build' ? './' : '/',
-    plugins: [vue()],
-  }
+export default defineConfig({
+  base: '/PA-Dashboard/',
+  plugins: [vue()],
 })
